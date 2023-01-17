dae98baaaad9b8be598b890bb93cffa305de4a3c
sharad2512 <shnarute95@gmail.com>
2023-01-17 14:45:33 +0530

    ADDED : Code Of Valide User First Name Using Java Regex

--- src/main/java/com/bridgelabz/regex/UserRegistration.java
+++ src/main/java/com/bridgelabz/regex/UserRegistration.java
@@ -1,6 +1,27 @@
 package com.bridgelabz.regex;
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 public class UserRegistration {
+    public String FirstName(String name) {
+        String regex = "[A-Z][a-z]{2,}";
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(name);
+        if (matcher.matches()) {
+            return name;
+        }
+        String fail = "No match found";
+        return fail;
+    }
     public static void main(String[] args) {
         System.out.println("Welcome To Java Regex Program");
+        Scanner scan = new Scanner(System.in);
+        UserRegistration userRegistration = new UserRegistration();
+        String name;
+        System.out.println("Enter the first name With Starting First letter Should be in Capital:-");
+        name = scan.next();
+        String result = userRegistration.FirstName(name);
+        System.out.println(result);
     }
+
 }
